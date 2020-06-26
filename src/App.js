ab29dcda8074debfb170ac604fdb9f979baa46ff
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-06-26 20:47:54 +0530

    import StarRating for app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import StarRating from './StarRating';
 
 function App() {
   return (
     <div className="App">
-      
+      <StarRating />
     </div>
   );
 }
