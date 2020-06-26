bc4982fdae14ebb8d444ee48bdd60e3e2c768f75
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-06-26 20:57:31 +0530

    create array for print 5 stars

--- src/StarRating.js
+++ src/StarRating.js
@@ -2,7 +2,13 @@ import React from 'react'
 import { FaStar } from 'react-icons/fa'
 
 const StarRating = () => {
-    return <div><FaStar /></div>
+    return (
+        <div>
+            {[ ...Array(5)].map((star) => {
+                return <FaStar size={100} />
+            })}
+        </div>
+    );
 }
 
 export default StarRating
